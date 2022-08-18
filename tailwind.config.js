c128ad89f0cc4490a5c9fe921681e26f20b21b29
Shivaram0005 <shivaram8534@gmail.com>
2022-08-18 19:15:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      'inter':['Inter' , 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
